c0997791970b22e75ec9c0ca35b5bb3f38db16e1
Damien <db.webapp@gmail.com>
2023-01-08 16:42:18 +0000

    initial commit fixing error URL database

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -14,4 +12,3 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    
